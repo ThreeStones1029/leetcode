949c2798837f85182665e104581d7d0dca9c05b3
ThreeStones1029 <2320218115@qq.com>
2024-04-26 23:04:06 +0800

    update leetcode 150

--- 150.逆波兰表达式求值.cpp
+++ 150.逆波兰表达式求值.cpp
@@ -4,7 +4,7 @@
  * @Author: ThreeStones1029 2320218115@qq.com
  * @Date: 2024-04-26 22:39:59
  * @LastEditors: ShuaiLei
- * @LastEditTime: 2024-04-26 22:42:53
+ * @LastEditTime: 2024-04-26 23:01:35
  */
 /*
  * @lc app=leetcode.cn id=150 lang=cpp
@@ -17,10 +17,32 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int> number;
-        for (int i = 0; i < tokes.size(); i++){
-            if 
+        for (int i = 0; i < tokens.size(); i++){
+            if (tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "/"){
+                std::cout << "hello world" << std::endl;
+                int int_number = (int) tokens[i];
+                number.push(int_number);
+            }
+            else{
+                int second = number.top();
+                number.pop();
+                int first = number.top();
+                number.pop();
+                if (tokens[i] == "+"){
+                    number.push(first + second);
+                }
+                if (tokens[i] == "-"){
+                    number.push(first - second);
+                }
+                if (tokens[i] == "*"){
+                    number.push(first * second);
+                }
+                if (tokens[i] == "/"){
+                    number.push(first / second);
+                }
+            }
         }
-
+        return number.top();
     }
 };
 // @lc code=end
