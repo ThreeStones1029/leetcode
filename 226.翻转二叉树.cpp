00ddb23cbcdb3e218dfb6d2a766fd41b17045404
ThreeStones1029 <2320218115@qq.com>
2024-05-01 22:55:33 +0800

    update Binary tree

--- 226.翻转二叉树.cpp
+++ 226.翻转二叉树.cpp
@@ -4,7 +4,7 @@
  * @Author: ThreeStones1029 2320218115@qq.com
  * @Date: 2024-05-01 21:51:52
  * @LastEditors: ShuaiLei
- * @LastEditTime: 2024-05-01 22:42:57
+ * @LastEditTime: 2024-05-01 22:53:14
  */
 /*
  * @lc app=leetcode.cn id=226 lang=cpp
@@ -98,29 +98,57 @@
 // };
 
 // 前序遍历统一迭代法
-class Solution {
-public:
-    TreeNode* invertTree(TreeNode* root) {
-        if (root == NULL) return root;
-        invertTree(root->left);
-        swap(root->left, root->right);
-        // 重复翻转左子树,因为交换了
-        invertTree(root->left);
-        return root;
-    }
-};
-
-// 中序遍历统一迭代法
 // class Solution {
 // public:
 //     TreeNode* invertTree(TreeNode* root) {
 //         if (root == NULL) return root;
-//         invertTree(root->left);
-//         swap(root->left, root->right);
-//         // 重复翻转左子树,因为交换了
-//         invertTree(root->left);
+//         stack<TreeNode*> node_stack;
+//         node_stack.push(root);
+//         while (!node_stack.empty()){
+//             TreeNode* cur = node_stack.top();
+//             if (cur != NULL){
+//                 node_stack.pop();
+//                 if (cur->right != NULL) node_stack.push(cur->right); // 右
+//                 if (cur->left != NULL) node_stack.push(cur->left); // 左
+//                 node_stack.push(cur);                                     // 中
+//                 node_stack.push(NULL);
+//             }
+//             else{
+//                 node_stack.pop();
+//                 cur = node_stack.top();
+//                 swap(cur->right, cur->left);
+//                 node_stack.pop();
+//             }
+//         }
 //         return root;
 //     }
 // };
+
+// 中序遍历统一迭代法
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == NULL) return root;
+        stack<TreeNode*> node_stack;
+        node_stack.push(root);
+        while (!node_stack.empty()){
+            TreeNode* cur = node_stack.top();
+            if (cur != NULL){
+                node_stack.pop();
+                if (cur->right != NULL) node_stack.push(cur->right); // 右
+                node_stack.push(cur);                              // 中
+                node_stack.push(NULL);
+                if (cur->left != NULL) node_stack.push(cur->left); // 左
+            }
+            else{
+                node_stack.pop();
+                cur = node_stack.top();
+                swap(cur->right, cur->left);
+                node_stack.pop();
+            }
+        }
+        return root;
+    }
+};
 // @lc code=end
 
