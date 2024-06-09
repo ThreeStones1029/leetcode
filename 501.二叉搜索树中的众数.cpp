80f3c8c4ff298dd8897f85aeb9f2c74a13698316
ThreeStones1029 <2320218115@qq.com>
2024-06-09 15:39:32 +0800

    update leetcode 501

--- 501.二叉搜索树中的众数.cpp
+++ 501.二叉搜索树中的众数.cpp
@@ -12,7 +12,7 @@ using namespace std;
  * @Author: ThreeStones1029 2320218115@qq.com
  * @Date: 2024-06-05 16:33:00
  * @LastEditors: ShuaiLei
- * @LastEditTime: 2024-06-05 17:18:35
+ * @LastEditTime: 2024-06-09 15:33:30
  */
 /*
  * @lc app=leetcode.cn id=501 lang=cpp
@@ -32,20 +32,20 @@ using namespace std;
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// 中序遍历统一迭代法
 class Solution {
 public:
-    void middle_order_traversal(TreeNode* root, vector<int>& result) {
+    // 统一迭代中序遍历
+    void unified_iteration_middle_order_traversal(TreeNode* root, vector<int>& result) {
         stack<TreeNode*> node_stack;
         node_stack.push(root);
         while (!node_stack.empty()) {
             TreeNode* cur = node_stack.top();
             if (cur != NULL) {
                 node_stack.pop();
-                if (node->right != NULL) node_stack.push(root->right);
+                if (cur->right != NULL) node_stack.push(cur->right);
                 node_stack.push(cur);
                 node_stack.push(NULL);
-                if (node->left != NULL) node_stack.push(root->left);
+                if (cur->left != NULL) node_stack.push(cur->left);
             }
             else {
                 node_stack.pop();
@@ -55,36 +55,118 @@ public:
             }
         }
     }
+    
+    // 递归法中序遍历
+    void recursion_middle_order_traversal(TreeNode* root, vector<int>& result) {
+        if (root->left != NULL) recursion_middle_order_traversal(root->left, result);
+        if (root != NULL) result.push_back(root->val);
+        if (root->right != NULL) recursion_middle_order_traversal(root->right, result); 
+    }
+
+    // 迭代法中序遍历
+    void iteration_middle_order_traversal(TreeNode* root, vector<int>& result) {
+        stack<TreeNode*> node_stack;
+        TreeNode* cur = root;
+        while (cur != NULL || !node_stack.empty()) {
+            if (cur != NULL) {
+                node_stack.push(cur);
+                cur = cur->left;
+            }
+            else {
+                cur = node_stack.top();
+                node_stack.pop();
+                result.push_back(cur->val);
+                cur = cur->right;
+            }
+        }
+    }
 
 
-    vector<int> findMode(TreeNode* root) {
+    vector<int> findMode1(TreeNode* root) {
         vector<int> tree_vals;
         vector<int> result;
         unordered_map<int, int> result_map;
-        middle_order_traversal(root, tree_vals);
+        // unified_iteration_middle_order_traversal(root, tree_vals); // 统一迭代中序遍历
+        // recursion_middle_order_traversal(root, tree_vals);// 递归法中序遍历
+        iteration_middle_order_traversal(root, tree_vals);
         int num = 1;
-        if (tree_vals.size() == 1) return tree_vals[0];
-        for (int i = 1; i < tree_vals.size(); i++) {
-            if (tree_vals[i] != tree_vals[i - 1]) {
-                unordered_map[tree_vals[i - 1]] = num;
-                num = 1;
+        for (int i = 0; i < tree_vals.size(); i++) {
+            auto it = result_map.find(tree_vals[i]);
+            if (it != result_map.end()) {
+                result_map[tree_vals[i]]++;
             }
             else {
-                num += 1;
+                result_map[tree_vals[i]] = 1;
             }
         }
-        // 先找到最大众数
-        int max = result[0];
-        for (int i = 0; i < result.size(); i++) {
-            if (result[i] > max) max = result[i];
-        }
-        //判断是否有其余众数
-        for (int i = 0; i < result.size(); i++) {
 
+        auto max_pair = result_map.begin();
+        // 迭代映射，寻找值最大的键
+        for (auto it = result_map.begin(); it != result_map.end(); ++it) {
+            if (it->second > max_pair->second) {
+                max_pair = it;
+            }
         }
-        return num;
-
+        result.push_back(max_pair->first);
+        for (auto it = result_map.begin(); it != result_map.end(); ++it) {
+            if (it->second == max_pair->second && it->first != max_pair->first) {
+                result.push_back(it->first);
+            }
+        }
+        return result;
     }
+
+
+    vector<int> findMode2(TreeNode* root) {
+        vector<int> tree_vals;
+        vector<int> result;
+        
+        // unified_iteration_middle_order_traversal(root, tree_vals); // 统一迭代中序遍历
+        // recursion_middle_order_traversal(root, tree_vals);// 递归法中序遍历
+        iteration_middle_order_traversal(root, tree_vals);
+        int cur_val = tree_vals[0];
+        int num = 1;
+        int vals_max_num = 1;
+        for (int i = 1; i < tree_vals.size(); i++) {
+            if (tree_vals[i] == cur_val) {
+                num++;
+            }
+            else {
+                if (num > vals_max_num) {
+                    // 若大于当前结果里面的数对应的数量，则全部删除后加入
+                    while (result.size() > 0)
+                    {
+                       result.pop_back();
+                    }
+                    result.push_back(cur_val);
+                    vals_max_num = num;
+                }else if (num == vals_max_num){
+                    result.push_back(cur_val);
+                }
+                cur_val = tree_vals[i];
+                num = 1;
+            }
+        }
+        // 处理最后的一个元素
+        if (num > vals_max_num) {
+            while (result.size() > 0)
+            {
+                result.pop_back();
+            }
+            result.push_back(cur_val);
+        }
+        if (num == vals_max_num) {
+            result.push_back(cur_val);
+        }
+
+        return result;
+    }
+
+    vector<int> findMode(TreeNode* root) {
+        // return findMode1(root); // 第一种求众数
+        return findMode2(root); // 第二种求众数
+    }
+
 };
 // @lc code=end
 
