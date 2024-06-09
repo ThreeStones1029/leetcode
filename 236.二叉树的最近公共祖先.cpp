0fa38ec1e07b08f8abc21836ea4af6b3e061a083
ThreeStones1029 <2320218115@qq.com>
2024-06-09 22:51:56 +0800

    update leetcode

--- 236.二叉树的最近公共祖先.cpp
+++ 236.二叉树的最近公共祖先.cpp
@@ -1,10 +1,18 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem236.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
 /*
  * @Description: 
  * @version: 
  * @Author: ThreeStones1029 2320218115@qq.com
  * @Date: 2024-06-09 15:38:40
  * @LastEditors: ShuaiLei
- * @LastEditTime: 2024-06-09 15:51:33
+ * @LastEditTime: 2024-06-09 22:25:30
  */
 /*
  * @lc app=leetcode.cn id=236 lang=cpp
@@ -25,8 +33,18 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
-        
+        if (root == NULL) return root;
+        if (root == p || root == q) return root;
+        // 后续遍历
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (left != NULL && right != NULL) return root;
+        else if (left == NULL && right != NULL){
+            return right;
+        } else if (left != NULL && right == NULL){
+            return left;
+        }
+        else return NULL;
     }
 };
 // @lc code=end
