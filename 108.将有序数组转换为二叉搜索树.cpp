e1035bf232592fede351bcfcb68b470e6bb2aafc
ThreeStones1029 <2320218115@qq.com>
2024-06-13 23:18:21 +0800

    update leetcode

--- 108.将有序数组转换为二叉搜索树.cpp
+++ 108.将有序数组转换为二叉搜索树.cpp
@@ -12,7 +12,7 @@ using namespace std;
  * @Author: ThreeStones1029 2320218115@qq.com
  * @Date: 2024-06-11 22:48:57
  * @LastEditors: ShuaiLei
- * @LastEditTime: 2024-06-13 22:40:14
+ * @LastEditTime: 2024-06-13 23:17:53
  */
 /*
  * @lc app=leetcode.cn id=108 lang=cpp
@@ -51,10 +51,53 @@ public:
     }
 
     
-    TreeNode* sortedArrayToBST(vector<int>& nums) {
+    TreeNode* sortedArrayToBST1(vector<int>& nums) {
         TreeNode* root = NULL;
         root = transval(nums, 0, nums.size());
         return root;
+    }
+
+    // 代码随想录，迭代法，三个队列 左闭右闭
+    TreeNode* sortedArrayToBST2(vector<int>& nums) {
+        if (nums.size() == 0) return nullptr;
+
+        TreeNode* root = new TreeNode(0);   // 初始根节点
+        queue<TreeNode*> nodeQue;           // 放遍历的节点
+        queue<int> leftQue;                 // 保存左区间下标
+        queue<int> rightQue;                // 保存右区间下标
+        nodeQue.push(root);                 // 根节点入队列
+        leftQue.push(0);                    // 0为左区间下标初始位置
+        rightQue.push(nums.size() - 1);     // nums.size() - 1为右区间下标初始位置
+
+        while (!nodeQue.empty()) {
+            TreeNode* curNode = nodeQue.front();
+            nodeQue.pop();
+            int left = leftQue.front(); leftQue.pop();
+            int right = rightQue.front(); rightQue.pop();
+            int mid = left + ((right - left) / 2);
+
+            curNode->val = nums[mid];       // 将mid对应的元素给中间节点
+
+            if (left <= mid - 1) {          // 处理左区间
+                curNode->left = new TreeNode(0);
+                nodeQue.push(curNode->left);
+                leftQue.push(left);
+                rightQue.push(mid - 1);
+            }
+
+            if (right >= mid + 1) {         // 处理右区间
+                curNode->right = new TreeNode(0);
+                nodeQue.push(curNode->right);
+                leftQue.push(mid + 1);
+                rightQue.push(right);
+            }
+        }
+        return root;
+    }
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        // return sortedArrayToBST1(nums);
+        return sortedArrayToBST2(nums);
     }
 };
 // @lc code=end
