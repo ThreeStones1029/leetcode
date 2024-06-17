7f3ff6c569e5b5ef5a970e99213726e85e400a6e
ThreeStones1029 <2320218115@qq.com>
2024-06-17 23:05:08 +0800

    update leetcode

--- 77.组合.cpp
+++ 77.组合.cpp
@@ -1,10 +1,18 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem77.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
 /*
  * @Description:
  * @version: 
  * @Author: ThreeStones1029 2320218115@qq.com
  * @Date: 2024-06-16 16:02:41
  * @LastEditors: ShuaiLei
- * @LastEditTime: 2024-06-16 23:20:33
+ * @LastEditTime: 2024-06-17 23:03:22
  */
 /*
  * @lc app=leetcode.cn id=77 lang=cpp
@@ -14,31 +22,36 @@
 
 // @lc code=start
 class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(int n, int k, int startIndex) {
+        if (path.size() == k) {
+            result.push_back(path);
+            return;
+        }
+        for (int i = startIndex; i <= n - (k - path.size()) + 1; i++) { // 优化的地方
+            path.push_back(i); // 处理节点
+            backtracking(n, k, i + 1);
+            path.pop_back(); // 回溯，撤销处理的节点
+        }
+    }
+
+    void backtracking2(int n, int k, int startIndex) {
+        if (path.size() == k) {
+            result.push_back(path);
+        }
+        for (int i = startIndex; i <= n; i++) {
+            path.push_back(i);
+            backtracking2(n, k, i+1);
+            path.pop_back();
+        }
+    }
+
 public:
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> results;
-        vector<int> sub_result;
-        int m = 1;
-        for (int i = 1; i <= n;) {
-            sub_result.push_back(i);
-            while (m < k && i + k <= n + 1) {
-                for (int j = i + m; j <= n; j++) {
-                    sub_result.push_back(j);
-                    break;
-                }
-                if (m == k - 1) {
-                    results.push_back(sub_result);
-                    // 回溯
-                    sub_result.pop_back();
-                    // sub_result.clear();
-                }
-                m++;
-            }
-            // 回溯
-            m = 1;
-            i++;
-        }
-        return results;
+        backtracking2(n, k, 1);
+        return result;
     }
 };
 // @lc code=end
