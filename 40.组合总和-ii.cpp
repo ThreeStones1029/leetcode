dab5d8957b9d57fccf0e09d193ae2b63a9318d9b
ThreeStones1029 <2320218115@qq.com>
2024-06-20 23:08:47 +0800

    update leetcode

--- 40.组合总和-ii.cpp
+++ 40.组合总和-ii.cpp
@@ -1,32 +1,24 @@
 // @before-stub-for-debug-begin
 #include <vector>
 #include <string>
-#include "commoncppproblem39.h"
+#include "commoncppproblem40.h"
 
 using namespace std;
 // @before-stub-for-debug-end
 
 /*
- * @Description: 
- * @version: 
- * @Author: ThreeStones1029 2320218115@qq.com
- * @Date: 2024-06-19 23:13:48
- * @LastEditors: ShuaiLei
- * @LastEditTime: 2024-06-20 22:21:53
- */
-/*
- * @lc app=leetcode.cn id=39 lang=cpp
+ * @lc app=leetcode.cn id=40 lang=cpp
  *
- * [39] 组合总和
+ * [40] 组合总和 II
  */
 
 // @lc code=start
-
 class Solution {
 private:
     vector<vector<int>> result;
     vector<int> path;
     int sum = 0;
+
     // 比较两个数组是否一样
     bool issame_vector(vector<int> array1, vector<int> array2) {
         sort(array1.begin(), array1.end());
@@ -42,11 +34,8 @@ private:
         else return false;
     }
 
-    // 暴力解法 会超时,原因在于时间过多的用在排除重复组合
-    void backtracking(vector<int>& candidates, int target) {
-        if (sum > target) {
-            return;
-        }
+    void backtracking(vector<int>& candidates, int target, int start) {
+        if (sum > target) return;
         else if (sum == target) {
             // 判断是否有一样的数组，没有则加入
             bool is_exist = false;
@@ -57,38 +46,40 @@ private:
             return;
         }
         else {
-            for (int i = 0; i < candidates.size(); i++) {
+            for (int i = start; i < candidates.size(); i++) {
                 sum += candidates[i];
                 path.push_back(candidates[i]);
-                backtracking(candidates, target);
+                backtracking(candidates, target, i + 1);
                 sum -= candidates[i];
                 path.pop_back();
             }
         }
     }
 
-    // 加入开始下标，每种情况表示相当于固定不同的数字个数
     void backtracking1(vector<int>& candidates, int target, int start) {
-        if (sum > target) {
-            return;
-        }
+        if (sum > target) return;
         else if (sum == target) {
             result.push_back(path);
-            return;
         }
         else {
             for (int i = start; i < candidates.size(); i++) {
+                // 要对同一树层使用过的元素进行跳过
+                if (i > start && candidates[i] == candidates[i - 1]) {
+                    continue;
+                }
                 sum += candidates[i];
                 path.push_back(candidates[i]);
-                backtracking1(candidates, target, i);
+                backtracking1(candidates, target, i + 1);
                 sum -= candidates[i];
                 path.pop_back();
             }
         }
     }
-    
+
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        // backtracking(candidates, target, 0);
+        sort(candidates.begin(), candidates.end());
         backtracking1(candidates, target, 0);
         return result;
     }
